a125d4542ca157a694c31baabdf4258fb0964006
Lil Puzo <92866229+Lifehucker@users.noreply.github.com>
2025-03-26 11:22:52 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ document.addEventListener('DOMContentLoaded', function () {
     const pages = document.querySelectorAll('.page');
     const links = document.querySelectorAll('nav a');
 
-    // Функция для переключения страниц
     function switchPage(event) {
         event.preventDefault();
         const targetPage = event.target.getAttribute('data-page');
